d48aa1c8d654da00e5ff256653f362afde428d7d
Andrew Hrechkan <andrewhrechkan@gmail.com>
2021-11-21 22:36:22 +0200

    updating link on Linkedin

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -13,7 +13,7 @@ function Social() {
             <Image className={style.img} src='/images/social/telegram.png' alt='telegram' />
          </a>
          <a
-            href="https://www.linkedin.com/in/%D0%B0%D0%BD%D0%B4%D1%80%D0%B5%D0%B9-%D0%B3%D1%80%D0%B5%D1%87%D0%BA%D0%B0%D0%BD%D1%8C-661322224/"
+            href="https://www.linkedin.com/in/andrii-hrechkan-661322224/"
             target="_blank"
             rel="noopener noreferrer"
          >
